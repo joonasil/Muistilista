dc4415a89d8c5965eb977c4c1f624a7d26940a11
joonasil <joonas.ilvonen@outlook.com>
2016-04-04 22:30:03 +0300

    HTML opettelua

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,17 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  View::make('suunnitelmat/frontpage.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
     }
+    
+    public static function login(){
+        View::make('suunnitelmat/login.html');
+    }
+    
+    
   }
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,25 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Account(
+    id SERIAL PRIMARY KEY,
+    username varchar(20) NOT NULL,
+    password varchar(20) NOT NULL
+);
+
+CREATE TABLE Errand(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES Account(id),
+    description varchar(200) NOT NULL,
+    priority INTEGER NOT NULL
+    status boolean DEFAULT FALSE,
+    deadline DATE
+);
+
+CREATE TABLE Category(
+    id SERIAL PRIMARY KEY,
+    category_name varchar(20) NOT NULL
+);
+
+CREATE TABLE Categories(
+    errand_id INTEGER REFERENCES Errand(id),
+    category_id INTEGER REFERENCES Category(id)
+);
